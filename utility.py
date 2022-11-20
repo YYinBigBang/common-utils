86b06671fdaeb59f89a5b5915073aad4e88f4a8c
YYinBigBang <yy32438084@gmail.com>
2022-11-21 01:36:05 +0800

    tools version 1.1

--- utility.py
+++ utility.py
@@ -1,34 +1,32 @@
-"""development tools
-
-"""
-
-
-from pathlib import Path
+"""development tools"""
 import logging
 import sys
 import os
 import time
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-stdout_handler = logging.StreamHandler(sys.stdout)
-logger.addHandler(stdout_handler)
-# output Logger file
-log_path = './log'
-log_name = 'utility.log'
-log_file = f'{log_path}/{log_name}'
-os.makedirs(log_path, exist_ok=True)
-file_handler = logging.FileHandler(log_file)
-formatter = logging.Formatter("%(asctime)s - %(levelname)s: %(message)s")
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
+def log():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    logger.addHandler(stdout_handler)
+    # output Logger file
+    log_path = './log_file'
+    log_name = 'utility.log'
+    log_file = f'{log_path}/{log_name}'
+    os.makedirs(log_path, exist_ok=True)
+    file_handler = logging.FileHandler(log_file)
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s: %(message)s")
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
 
-# logger.removeHandler(file_handler)
+    return logger
+
+
 
 def timelog(callback):
-    """calculate execution time
-    """
+    """calculate execution time"""
     def wrapper(arg = None):
+        logger = log()
         try:
             # reocrd start time
             cpu_time_start = time.process_time() # Not include time.sleep()
@@ -50,4 +48,4 @@ def timelog(callback):
     wrapper.__name__ = callback.__name__
     return wrapper
 
-
+#TODO:添加timeout功能(將函數式限制執行時間)
